b98ac7d669198232910306ac3ee2b2af3b20a0ec
fwjensen <fredrik.wigsnes@hotmail.com>
2015-10-16 21:54:57 +0200

    Update and rename Tile.java to Wall.java

---
+++ tile/Wall.java
@@ -0,0 +1,15 @@
+package tile;
+
+import javafx.scene.paint.Color;
+
+public class Wall extends Tile {
+
+	public Wall(int X, int Y, Color color) {
+		super(X, Y, color);
+	}
+
+	@Override
+	public void moveHere() {
+		
+	}
+}
