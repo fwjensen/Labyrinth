7a06ef3412fd8e558451ba5022b7e8bcf0527ae0
fwjensen <fredrik.wigsnes@hotmail.com>
2015-10-16 21:53:01 +0200

    Create Tile.java

---
+++ tile/Tile.java
@@ -0,0 +1,15 @@
+package tile;
+
+import javafx.scene.paint.Color;
+
+public class Road extends Tile {
+
+	public Road(int X, int Y, Color color) {
+		super(X, Y, color);
+	}
+
+	@Override
+	public void moveHere() {
+		
+	}
+}
